7b30f3bcb72759fee302bf3751487802e037d031
Micaela Tamires <micaela.tamir@gmail.com>
2025-11-19 15:05:39 -0300

    Conectando a banco de dados no sistema

---
+++ src/Conexao.java
@@ -0,0 +1,23 @@
+Package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:postgresql://localhost:5432/sistema_de_login_com_senha_criptografada";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "977310607";
+
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar com o banco de dados: " + e.getMessage());
+            return null;
+
+        }
+
+    }
+}
---
+++ src/UsuarioDAO.java
@@ -0,0 +1,6 @@
+package  dao;
+import model.Usuarios;
+import java.sql.PreparedStatement;
+
+public class UsuarioDAO {
+}
