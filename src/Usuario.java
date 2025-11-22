fe3d371086982f218f8f3004a455986c362c41fc
Micaela Tamires <micaela.tamir@gmail.com>
2025-11-21 23:30:57 -0300

    Criando classe Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,6 +1,13 @@
 package  model;
 
-public class Uusario {
+import java.time.LocalDate;
+
+public class Usuario {
+
+    private int id;
     private String nome;
     private String email;
+    private String senha_hash;
+    private LocalDate data_cadastro;
+
 }
