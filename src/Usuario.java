9410af8080c0747890760bd5ece7df5f0d0170c3
Micaela Tamires <micaela.tamir@gmail.com>
2025-11-29 17:35:16 -0300

    Atualização completa do model Usuario
    - Corrigido tipo da senhaNormal (de int para String).
    - Adicionado campo senha_hash para armazenamento do hash da senha no banco.
    - Incluídos todos os getters e setters necessários.
    - Adicionado campo data_cadastro do tipo LocalDate.
    - Estrutura do model finalizada para integração com o UsuarioDAO.

--- src/Usuario.java
+++ src/Usuario.java
@@ -7,7 +7,45 @@ public class Usuario {
     private int id;
     private String nome;
     private String email;
+    private String senhaNormal;
     private String senha_hash;
     private LocalDate data_cadastro;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenhaNormal() {
+        return senhaNormal;
+    }
+    public void setSenhaNormal(String senhaNormal) {
+        this.senhaNormal = senhaNormal;
+    }
+    public String getSenha_hash() {
+        return senha_hash;
+    }
+    public void setSenha_hash(String senha_hash) {
+        this.senha_hash = senha_hash;
+    }
+    public LocalDate getData_cadastro() {
+        return data_cadastro;
+    }
+    public void setData_cadastro(LocalDate data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
 
 }
